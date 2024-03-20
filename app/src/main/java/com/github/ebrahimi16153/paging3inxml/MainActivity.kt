82c3c500764d97f3549816bd04a0e0b8909ffe5e
Mohammad <ebrahimi2723@yahoo.com>
2024-03-20 18:57:02 +0330

    1 -> add and implemented loading view by PagingDataAdapter 2 -> add and implemented  swipe to refresh

--- app/src/main/java/com/github/ebrahimi16153/paging3inxml/MainActivity.kt
+++ app/src/main/java/com/github/ebrahimi16153/paging3inxml/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.github.ebrahimi16153.paging3inxml
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
@@ -14,7 +11,6 @@ import com.github.ebrahimi16153.paging3inxml.adapter.MovieAdapter
 import com.github.ebrahimi16153.paging3inxml.databinding.ActivityMainBinding
 import com.github.ebrahimi16153.paging3inxml.viewmodel.MovieViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
